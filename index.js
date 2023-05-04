aa6f1721d64a503523e97a9ef9d7598e0d24fcdf
devmunjal1 <dev@braina.live>
2023-05-04 11:05:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(express.static("ui"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    console.log(req.path);
+    var type = req.path.split("/")[2];
+    cb(null, type); // specify the destination directory
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname); // specify the filename
+  },
+});
+
+// create the multer instance
+const upload = multer({ storage: storage });
+
+app.listen("3001", () => {
+  console.log("Server Started On PORT: 3000");
+});
+
+app.post("/store/:type", upload.single("image"), function (req, res) {
+  res.send("File uploaded successfully!");
+});
